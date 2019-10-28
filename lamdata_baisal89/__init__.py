2e89f2b845491db2966a4e95e14d03a5fba7e6a6
norashakirova <b.ergeshev@gmail.com>
2019-10-28 17:59:28 -0400

    I have added few files

---
+++ lamdata_baisal89/__init__.py
@@ -0,0 +1,16 @@
+"""
+Lambda - a collection  of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+#sample code
+
+#samep dataset
+ONES = pd.Dataframe(np.ones(10))
+ZEROS = pd.Dataframe(np.zeros(50))
+
+#sample functions
+def increment(x):
+    return(x + 1)
